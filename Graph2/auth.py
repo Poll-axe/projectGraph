aa086ab83eee7d31ba02ca0b81b0800a1ddbd21e
Poll-axe <pronin.vladimir.forjob@gmail.com>
2018-10-02 23:53:27 +0300

    add

---
+++ Graph2/auth.py
@@ -0,0 +1,14 @@
+"""
+
+примеры id
+122541914
+336081407
+186553202
+440350634
+346219128
+Cсылка для получения ключа доступа
+https://oauth.vk.com/authorize?client_id=6498822&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.52
+
+"""
+# https://oauth.vk.com/authorize?client_id=6498822&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.52
+access_token = '7d911e827b9016ae5f5d24d0935dca900be9e8a7cc3999747e3e4c6500514b2c2265cd355724c83463526'
